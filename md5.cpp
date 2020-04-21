61c544a7a61c0f5401da1a56264297259032ebec
atarp <atarp22@googlemail.com>
2020-04-21 21:10:32 +0200

    modified:   md5.cpp

--- md5.cpp
+++ md5.cpp
@@ -4,10 +4,11 @@
 #include <iostream>
 
 
-int main(int argc, char** argv)
+int main([[maybe_unused]]int argc,[[maybe_unused]] char** argv)
 {
     Poco::MD5Engine md5;
     Poco::DigestOutputStream ds(md5);
+
     ds << "abcdefghijklmnopqrstuvwxyz";
     ds.close();
     std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
